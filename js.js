9b083c6cde7b55dd6b2ec8793ef8dc1182285776
esmewilding <esmewilding@gmail.com>
2025-12-22 19:34:52 +0000

    linked javascript to index.html added javascript to find the difference between bench start and end time

--- js.js
+++ js.js
@@ -1,2 +1,30 @@
-const form = document.getElementById("myForm");
-const formData = new FormData(form);
+function formSubmit() {
+    const formData = document.getElementById("myForm");
+
+    const stime = convert2jsDate(formData[0].value); // get value from form
+    const etime = convert2jsDate(formData[1].value);
+    const btime = formData[2].value;
+
+    const timeBtwn = getTimeBtwn(stime,etime);
+
+    document.getElementById("result").innerHTML = timeBtwn;
+}
+
+function convert2jsDate(timeValue) {
+     // split time value into hours and minutes
+    let [hours, minutes] = timeValue.split(":").map(Number);
+    let dateObject = new Date(1970, 0, 1, hours, minutes);
+    return dateObject;
+}
+
+function getTimeBtwn(stime,etime) {
+    let elapsed = etime - stime; // in milliseconds
+
+    if(elapsed < 0){ //if bench is started before midnight and finished after
+        etime.setDate(2);
+        elapsed = etime - stime;
+    }
+
+    elapsed = elapsed/(1000*60); //in minutes
+    return elapsed;
+}
